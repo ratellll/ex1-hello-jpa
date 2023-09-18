6bb7d874c88445ab0ce234650d69838de0259bec
ratellll <latellbin@naver.com>
2023-09-18 23:04:57 +0900

    feat: flush 는 쿼리나 커밋실행시 발동  수동으로 플러시를 먼저 호출해서 쿼리실행을 할수있음 (쓰기지연SQL까지 안가도됌 )

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,17 +18,11 @@ public class JpaMain {
         tx.begin();
 
         try { // 정석코드는 try catch해주는것 ,트랜잭션을 try안에다가 넣어주기
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setSize(275);
-            Member findMember2 = em.find(Member.class, 2L);
-            findMember2.setSize(245);
+            Member member = new Member(100L, "han" , 240);
+            em.persist(member);
+            em.flush();
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+            System.out.println("==============");
             tx.commit();
             //em.persist(member); //  트랜잭션부분 트랜잭션은 db상태를 변화시키기는 수행작업단위
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,6 +12,19 @@ public class Member {
     private Long id;
     private String name;
     private int size;
+
+
+    public Member (){
+
+    }
+
+
+    public Member(Long id, String name, int size) {
+        this.id = id;
+        this.name = name;
+        this.size = size;
+    }
+
     public Long getId() {
         return id;
     }
