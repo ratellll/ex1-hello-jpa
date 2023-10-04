0f0d0a58885d42ad8475a2babb7fb2802da5c0ff
ratellll <latellbin@naver.com>
2023-10-04 23:08:24 +0900

    feat: 기본 매핑연습 시간찍을땐 편하게 java LocalDateTime사용하기 Enum사용시 String 꼭붙여주기 ORDINAL사용시 꼬일수있음 Column매핑시 nullable이나 insertlable,updateable설정하기

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,29 +1,43 @@
 package hellojpa;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity // name이라는 속성이 있는데 클래스 이름을 웬만하면 따라감
 //@Table(name = "MB") 만약 Member말고 다른 테이블과 매핑하고싶을경우 name을 지정해줌 , catalog, schema로 설정가능
 public class Member {
 
 
-    @Id // PK가 뭔ㄴ지 선언해야함
+    @Id
     private Long id;
-    private String name;
-    private int size;
+    @Column(name = "name")
+    //insertble,updateble 은 insert나 update를할지 설정해놓는것
+    //nullable은 false를 주면 notNull로 들어가줌
+    //unique는 true로 설정시 유니크로 넣어줌 이 설정을 넣어주려면 해당 클래스에 주는게 나음
+    //columnDefinition은 내가 직접 설정을할수있음 ex)varchar(100)
+    private String username;
+    private Integer size;
+    @Enumerated(EnumType.STRING) //ENUM타입을 쓰고싶을때붙어준다 //ODINAL은 사용금지
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP) // 날짜타입  DATE, TIME, DATETIME  3가지가 있음
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP) // 잘 사용하지않음
+    private Date lastModifiedDate;
+    private LocalDateTime localDateTime; //이런식으로씀
+    @Lob //큰 컨텐츠를넣고싶을때 ex) varchar를 넘어선것 문자일경우 CLob  byte면 BLob
+    private String description;
 
-
-    public Member (){
+    public Member() {
 
     }
 
 
-    public Member(Long id, String name, int size) {
+    public Member(Long id, String username, int size) {
         this.id = id;
-        this.name = name;
+        this.username = username;
         this.size = size;
     }
 
@@ -35,12 +49,12 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String name) {
+        this.username = username;
     }
 
     public int getSize() {
