a15d36dc6a10f447726ba896a7c556eafc5902ae
ratellll <latellbin@naver.com>
2024-04-22 17:28:00 +0900

    feat: jpa 이론 error: h2 test.db가 존재하지않아 메인어플리케이션 실행오류

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,35 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import static jakarta.persistence.Persistence.createEntityManagerFactory;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManagerFactory emf = createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
         //code
 
-        em.close();
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("bin");
+            em.persist(member);
+            Member member2 = new Member();
+            member2.setId(2L);
+            member2.setName("sol");
+            em.persist(member2);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
         emf.close();
     }
 }
