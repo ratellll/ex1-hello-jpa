8dab6aa090240fc2e5074f80c36abc37b41a5145
ratellll <latellbin@naver.com>
2024-04-23 22:34:49 +0900

    feat: flush 이론

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,7 +16,7 @@ public class JpaMain {
         //code
 
         try {
-            //비영속
+           /** //비영속
             Member member = new Member();
             member.setId(1L);
             member.setName("bin");
@@ -30,6 +30,14 @@ public class JpaMain {
             member2.setName("sol");
             em.persist(member2);
 
+            tx.commit();**/
+
+            Member member = new Member(200L,"member1");
+            em.persist(member);
+            //db에 미리 반영하거나 쿼리를 확인하고싶으면 flush를 사용하여 강제호출
+            em.flush();
+
+            System.out.println("================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
