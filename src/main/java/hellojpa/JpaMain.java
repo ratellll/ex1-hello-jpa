3c34d8bb5333ad27caf7e3f5d9ec2cb8058fd210
ratellll <latellbin@naver.com>
2024-06-27 16:58:24 +0900

    feat: 매핑전략

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -7,46 +7,28 @@ import static jakarta.persistence.Persistence.createEntityManagerFactory;
 public class JpaMain {
 
     public static void main(String[] args) {
-
-        EntityManagerFactory emf = createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-
+        EntityManagerFactory emf = createEntityManagerFactory("hello");// db당 하나 서버실행시 딱하나만 실행된다고 보면됨
+        EntityManager em = emf.createEntityManager(); // 기능을 하나끝낼때마다 close
         EntityTransaction tx = em.getTransaction();
+
         tx.begin();
-        //code
 
         try {
-           /** //비영속
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("bin");
-            // setName까지 비영속상태 jpa랑 관계가 없기때문
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("빈이짱");
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("빈이");
+//            em.persist(member);
 
-            // persist를 시작하며 영속성 이때 db에 저장되어지진않음
-            em.persist(member);
-
-            Member member2 = new Member();
-            member2.setId(2L);
-            member2.setName("sol");
-            em.persist(member2);
-
-            tx.commit();**/
-
-            Member member = new Member(200L,"member1");
-            em.persist(member);
-            //db에 미리 반영하거나 쿼리를 확인하고싶으면 flush를 사용하여 강제호출
-            em.flush();
-
-            System.out.println("================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         }finally {
             em.close();
         }
+
+
         emf.close();
     }
-
-
-
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,92 +1,18 @@
 package hellojpa;
 
 
-import jakarta.persistence.*;
-
-import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
-//@SequenceGenerator(name = "member_seq_generator" , sequenceName = "member_seq"
-//  allocationSize = 50  기본으로 nextCall로 시퀀스를 불러올때 50개를 다 셋팅해놓고(미리 올려놓는것 50개의 시퀀스를) 사용을하는것 )
-
-//@TableGenerator(name = "MEMBER_SEQ_GENERATOR",
- //table = "MY_SEQUENCES",
- //pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
-    @Id //Id를 직접할당 (db에서 사용되는id)
-    //@GeneratedValue Id를 자동생성시켜줌
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) 기본 키 생성을 데이터베이스에 위임하는것
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_seq_generator") 시퀀스사용 (자동생성되는것)
-    //@GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR") 테이블 전략매핑 (시퀀스전용 테이블을 만드는것)
+    @Id
     private Long id;
-    @Column(name = "name")
-    private String username;
-    private Integer age;
 
-    @Enumerated(EnumType.STRING) //db에서 Enum을 쓰고싶을때 사용
-    private RoleType roleType;
+    private String name;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-
-    public Member() {
-
-    }
-
-
-    public Member(Long id, String name, Integer age) {
-        this.id = id;
-        this.username = name;
-        this.age = age;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     public Long getId() {
         return id;
@@ -96,11 +22,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setName(String name) {
+        this.name = name;
     }
 }
